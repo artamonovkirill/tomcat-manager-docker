24cb0e6c15c265d990e74ce7b924b58d15564237
Kirill Artamonov <artamonov.kirill@gmail.com>
2020-07-17 14:20:52 +0200

    :green_heart: basic auth

--- src/test/groovy/TomcatSpec.groovy
+++ src/test/groovy/TomcatSpec.groovy
@@ -1,9 +1,9 @@
 import org.testcontainers.containers.GenericContainer
-import org.testcontainers.containers.wait.strategy.Wait
 import spock.lang.Specification
 import spock.lang.Unroll
 
 import static org.testcontainers.containers.BindMode.READ_ONLY
+import static org.testcontainers.containers.wait.strategy.Wait.forLogMessage
 
 class TomcatSpec extends Specification {
 
@@ -13,16 +13,18 @@ class TomcatSpec extends Specification {
     def 'starts Tomcat #version with a manager application'() {
         given:
         def container = new GenericContainer("tomcat:$version")
-                .withExposedPorts(port) // for debug purposes
+                .withExposedPorts(port)
                 .withClasspathResourceMapping('manager.xml', '/usr/local/tomcat/conf/Catalina/localhost/manager.xml', READ_ONLY)
                 .withClasspathResourceMapping('users.xml', '/usr/local/tomcat/conf/tomcat-users.xml', READ_ONLY)
-                .waitingFor(Wait.forHttp('/manager/status'))
+                .waitingFor(forLogMessage(/.*Server startup in [\[]?[0-9]+[\]]? (ms|milliseconds).*/, 1))
 
         when:
         container.start()
 
         then:
-        noExceptionThrown()
+        "http://admin@${container.containerIpAddress}:${container.getMappedPort(port)}/manager/status"
+            .toURL()
+            .text
 
         cleanup:
         container.stop()
